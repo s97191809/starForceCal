c17071ab98212b2992f67d6cf9feb12d98aff4c5
c_sb <opt1997@gmail.com>
2023-01-05 00:25:39 +0900

    인트로 페이지 제작 및 수정

---
+++ app/src/main/java/com/kopractice/starforcecalculator/introActivity.kt
@@ -0,0 +1,27 @@
+package com.kopractice.starforcecalculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.kopractice.starforcecalculator.databinding.ActivityIntroBinding
+import com.kopractice.starforcecalculator.view.MainActivity
+
+class introActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        viewBinding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+
+        Glide.with(this).load(R.raw.pino).override(500,500).into(viewBinding.pinoGif)
+
+
+    }
+
+}
